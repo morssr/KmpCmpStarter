bfe30344556d82c428295924bb533c94800a421a
Mor Shaar <morsrs.dev@gmail.com>
2025-03-17 13:12:34 +0200

    add dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,12 +1,15 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.sqldelight)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -15,6 +18,9 @@ kotlin {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
+
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
     }
     
     listOf(
@@ -29,20 +35,55 @@ kotlin {
     }
     
     sourceSets {
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.ktor.okhttp)
+            implementation(libs.sqldelight.driver.android)
+            implementation(libs.koin.android)
         }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.darwin)
+            implementation(libs.sqldelight.driver.ios)
+        }
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.bundles.ktor)
+            implementation(libs.bundles.coil)
+            implementation(libs.bundles.koin)
+            implementation(libs.sqldelight.coroutines)
+            implementation(libs.kotlinx.serialization)
+            implementation(libs.kotlinx.dateTime)
+            implementation(libs.bundles.kermit)
+            implementation(libs.jetpack.navigation)
+            implementation(libs.jetpack.viewmodel)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlin.test.coroutine)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+            implementation(libs.ktor.test)
+            implementation(libs.koin.test)
+        }
+
+        androidUnitTest.dependencies {
+            implementation(libs.sqldelight.driver.jvm)
+        }
+
+        iosTest.dependencies {
+            implementation(libs.sqldelight.driver.ios)
         }
     }
 }
@@ -76,5 +117,14 @@ android {
 
 dependencies {
     debugImplementation(compose.uiTooling)
+    androidTestImplementation(libs.androidx.test.compose.junit4)
+    debugImplementation(libs.androidx.test.compose.manifets)
 }
 
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.kmp.cmp.app")
+        }
+    }
+}

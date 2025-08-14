afeacb696aaccf02f75e5c8f7bad613cecaacca6
Mor Shaar <morsrs.dev@gmail.com>
2025-08-14 18:24:36 +0300

    refactor: add missing imports

--- composeApp/src/commonMain/kotlin/com/kmp/cmp/app/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/cmp/app/presentation/home/HomeScreen.kt
@@ -1,22 +1,29 @@
 package com.kmp.cmp.app.presentation.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.kmp.cmp.app.Greeting
 import kmpcmpstarter.composeapp.generated.resources.Res
-import kmpcmpstarter.composeapp.generated.resources.random_users
-import kmpcmpstarter.composeapp.generated.resources.resources
+import kmpcmpstarter.composeapp.generated.resources.click_me
+import kmpcmpstarter.composeapp.generated.resources.compose
+import kmpcmpstarter.composeapp.generated.resources.compose_multiplatform
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 

2f3cb07b1a634553572c9c77574077741ffd3655
Mor Shaar <morsrs.dev@gmail.com>
2025-08-15 00:57:46 +0300

    build: add icons deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,6 +57,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(compose.materialIconsExtended)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.bundles.ktor)
